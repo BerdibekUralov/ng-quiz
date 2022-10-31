2e6b191f30a43f0996d77f5a7dca400c56d4720d
BerdibekUralov <uralov0212@gmail.com>
2022-10-31 05:45:02 +0300

    version 1.0.0

---
+++ src/app/question/question.component.ts
@@ -0,0 +1,110 @@
+import { Component, OnInit } from '@angular/core';
+import { interval } from 'rxjs';
+import { QuestionService } from '../service/question.service';
+
+@Component({
+  selector: 'app-question',
+  templateUrl: './question.component.html',
+  styleUrls: ['./question.component.scss']
+})
+export class QuestionComponent implements OnInit {
+
+  public name: string = ""
+  public questionList: any = []
+  public currentQuestion: number = 0
+  public points: number = 0
+  counter = 60
+  carrectAnswer: number = 0
+  inCorrectAnswer: number = 0
+  interval$:any;
+  progress: string = "0"
+  isQuizCompeted: boolean = false
+
+  constructor(private questionService: QuestionService) { }
+
+  ngOnInit(): void {
+    this.name = localStorage.getItem("name")!
+    this.getAllQuestions()
+    this.startCounter()
+  }
+
+  getAllQuestions() {
+    this.questionService.getQuestionJson()
+      .subscribe(res => {
+        this.questionList = res.question
+      })
+  }
+
+  nextQuestion(){
+    this.currentQuestion++
+  }
+  prevQuestion(){
+    this.currentQuestion--
+  }
+
+  answer(currentQno: number, option: any) {
+    if(currentQno === this.questionList.length){
+      setTimeout(() => {
+        this.isQuizCompeted = true
+      }, 1000)
+      this.stopCounter()
+    }
+    if(option.currect) {
+      this.points += 10
+      this.carrectAnswer++
+      setTimeout(() => {
+        this.currentQuestion++
+        this.resetCounter()
+        this.getProgressPercent()
+      }, 500)
+    }else{
+      this.inCorrectAnswer++
+      setTimeout(() => {
+        this.currentQuestion++
+        this.resetCounter()
+        this.getProgressPercent()
+      }, 500)
+      this.points -= 10
+    }
+  }
+
+  startCounter() {
+    this.interval$ = interval(1000)
+      .subscribe(val => {
+        this.counter--
+        if(this.counter === 0) {
+          this.currentQuestion++
+          this.counter = 60
+          this.points -= 10
+        }
+      })
+    setTimeout(() => {
+      this.interval$.unsubscribe()
+    }, 600000)
+  }
+
+  stopCounter() {
+    this.interval$.unsubscribe()
+    this.counter = 0
+  }
+
+  resetCounter() {
+    this.stopCounter()
+    this.counter = 60
+    this.startCounter()
+  }
+
+  resetGame() {
+    this.resetCounter()
+    this.getAllQuestions()
+    this.points = 0
+    this.counter = 60
+    this.currentQuestion = 0
+    this.progress = "0"
+  }
+
+  getProgressPercent() {
+    this.progress = ((this.currentQuestion/this.questionList.length)*100).toString()
+    return this.progress
+  }
+}
---
+++ src/app/welcome/welcome.component.ts
@@ -0,0 +1,28 @@
+import { Component, OnInit, ViewChild, ElementRef, } from '@angular/core';
+
+@Component({
+  selector: 'app-welcome',
+  templateUrl: './welcome.component.html',
+  styleUrls: ['./welcome.component.scss']
+})
+export class WelcomeComponent implements OnInit {
+
+  @ViewChild('name') nameKey !: ElementRef
+  public routeGame: string = ""
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  startGame(){
+    if(this.nameKey.nativeElement.value === ""){
+      alert("Enter Your Name, Please"),
+      this.routeGame= "welcome"
+    }
+    localStorage.setItem("name", this.nameKey.nativeElement.value)
+    this.routeGame = "question"
+
+  }
+
+}
